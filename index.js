edb8c9880472fbd24933c3fd5163b3cbf0f095c9
Milind Belhe <milindvb@gmail.com>
2018-10-31 14:48:07 +0000

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+const express = require('express')
+const app = express()
+
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+        const version = process.version
+        response.write("nodejs version --::: ");
+        response.write(version);
+        response.write(process.env.MB_API_URL);
+        response.end("");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
