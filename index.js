cc99cc909676d950aabf0dbf7504ffbd6cb28d0f
milindvb <milind141@yahoo.com>
2019-05-28 11:55:56 -0500

    addind content disposition

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ var path = require('path');
 var server = http.createServer(function(request, response) {
     response.setHeader('Connection', 'Transfer-Encoding');
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    response.setHeader('Content-Disposition', 'attachment; filename="sample23kb.txt"');
     response.setHeader('Transfer-Encoding', 'chunked');
     response.useChunkedEncodingByDefault = true;
     //response.writeHead(200, {"Content-Type": "text/plain"});
     response.writeHead(200);
     
 
-        var pid = process.pid;
+        //var pid = process.pid;
         //const argv = require('yargs').argv
 
         //var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
-        var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
-        fs.createReadStream(path.resolve(__dirname, 'sample23kb.txt')).pipe(response);
+        //var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
+        //fs.createReadStream(path.resolve(__dirname, 'sample23kb.txt')).pipe(response);
         //response.write(output);
         //response.end("---dev2---------");
 });
