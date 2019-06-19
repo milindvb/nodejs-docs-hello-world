6f4521227a2ad2f65b29d5011657f4876aa5781a
milindvb <milind141@yahoo.com>
2019-06-19 18:15:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var server = http.createServer(function(request, response) {
 
         //var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
         var output = "nodejs version = "+ process.version + ", Process Id = " + pid +" ---- " + request.headers['x-ms-client-principal-name'];
-        process.env['USERNAME'] = request.headers['x-ms-client-principal-name'];
+        process.env['USERNAME_AAD'] = request.headers['x-ms-client-principal-name'];
         response.write(output);
         response.end("------------");
 });
