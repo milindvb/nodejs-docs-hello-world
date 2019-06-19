be97e4df7d551030d4fb599840ee73c1c5fea6fe
milindvb <milind141@yahoo.com>
2019-06-19 17:18:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var server = http.createServer(function(request, response) {
         const argv = require('yargs').argv
 
         //var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
-        var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
+        var output = "nodejs version = "+ process.version + ", Process Id = " + pid +" ---- " + JSON.stringify(request.headers);
         response.write(output);
         response.end("------------");
 });
