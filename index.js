6710056bf19890a0f03cc0e6041d376e56b6a174
milindvb <mibelhe@microsoft.com>
2019-10-21 11:58:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var server = http.createServer(function(request, response) {
     //response.setHeader('Transfer-Encoding', 'chunked');
     //response.useChunkedEncodingByDefault = true;
     //response.writeHead(200, {"Content-Type": "text/plain"});
-    response.writeHead(200);
+    //response.writeHead(200);
     
 
         var pid = process.pid;
