35831a3507064fb279b472c9eb62750f174a1c9b
milindvb <mibelhe@microsoft.com>
2019-10-21 11:56:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var path = require('path');
 
 
 var server = http.createServer(function(request, response) {
-    response.setHeader('Connection', 'Transfer-Encoding');
+    //response.setHeader('Connection', 'Transfer-Encoding');
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    response.setHeader('Content-Disposition', 'attachment; filename="sample23kb.txt"');
-    response.setHeader('Transfer-Encoding', 'chunked');
-    response.useChunkedEncodingByDefault = true;
+    //response.setHeader('Content-Disposition', 'attachment; filename="sample23kb.txt"');
+    //response.setHeader('Transfer-Encoding', 'chunked');
+    //response.useChunkedEncodingByDefault = true;
     //response.writeHead(200, {"Content-Type": "text/plain"});
     response.writeHead(200);
     
@@ -19,7 +19,9 @@ var server = http.createServer(function(request, response) {
         //const argv = require('yargs').argv
 
         //var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
-        var output = JSON.stringify(request.headers)+ "nodejs version = "+ process.version + ", Process Id = " + pid;
+        //var output = JSON.stringify(request.headers)+ "nodejs version = "+ process.version + ", Process Id = " + pid;
+        var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
+
         //fs.createReadStream(path.resolve(__dirname, 'sample23kb.txt')).pipe(response);
         response.write(output);
         //response.end("---dev2---------");
