7b3f3f3ad08e1604e7d393f3b5386dc8d089d122
milindvb <mibelhe@microsoft.com>
2019-10-21 11:50:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
 
         const version = process.version
+        request.pipe(request('https://images.unsplash.com/photo-1565028832942-d005b1bd84f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')).pipe(reponse);
 
         response.write("nodejs version: ");
 
         response.write(version);
 
-        response.write(process.env.MB_API_URL);
+        //response.write(process.env.MB_API_URL);
 
         response.end("");
 
